d9b37ae50e5ab6d9512b03b59804f53ad9eef9af
unknown <gusrmuller@gmail.com>
2023-10-08 23:49:41 -0300

    add convenios section

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 nome: "Dr. Fabrício Rossi",
                 foto: "/images/random.png",
             },
-            {
-                nome: "Dra. Cardiologista 2",
-                foto: "/images/random.png",
-            }
         ],
         cardiologia: [
             {
@@ -36,7 +32,114 @@ document.addEventListener("DOMContentLoaded", function () {
                 foto: "/images/laismaia.png"
             }
         ],
-
+        endocrinologia: [
+            {
+                nome: "Dr. José Maria Cruz Machado",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        geriatria: [
+            {
+                nome: "Dra. Sarah Francisco",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        ginecologia: [
+            {
+                nome: "Dra. Nicole Kuster Porpino",
+                foto: "/images/laismaia.png"
+            },
+            {
+                nome: "Dr. Alvaro Lopes Vereno Filho",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        neurologia: [
+            {
+                nome: "Dra. Alba Valéria",
+                foto: "/images/laismaia.png"
+            },
+            {
+                nome: "Dr. Paulo de Paiva",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        nutricao: [
+            {
+                nome: "Marilza Dias",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        oftalmologia: [
+            {
+                nome: "Dr. Ricardo Cerqueira Lima",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        ortopedia: [
+            {
+                nome: "Dr. Sérgio Brickwedde",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        otorrino: [
+            {
+                nome: "Dra. Karina Brandão Lutz",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        pediatria: [
+            {
+                nome: "Dra. Kátia Faria Alves",
+                foto: "/images/laismaia.png"
+            },
+            {
+                nome: "Dra. Magda Lúcia Machado Pimentel",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        pneumologia: [
+            {
+                nome: "Dra. Amanda Schwambach Velten",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        psicologia: [
+            {
+                nome: "Fernanda de Biase",
+                foto: "/images/laismaia.png"
+            },
+            {
+                nome: "Solange Lucena Mendes",
+                foto: "/images/laismaia.png"
+            },
+            {
+                nome: "Adriania Rodrigues",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        psiquiatria: [
+            {
+                nome: "Dr. Ricardo Herkenhoff Araujo",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        radiologia: [
+            {
+                nome: "Dr. Plinio Zanello",
+                foto: "/images/laismaia.png"
+            },
+            {
+                nome: "Dr. Rubens Littig",
+                foto: "/images/laismaia.png"
+            }
+        ],
+        urologia: [
+            {
+                nome: "Dr. Leandro Correa Leal",
+                foto: "/images/laismaia.png"
+            }
+        ]
     };
 
     especialidades.forEach((especialidade) => {
@@ -49,7 +152,7 @@ document.addEventListener("DOMContentLoaded", function () {
             document.body.style.overflowY = "hidden";
             medicosContainer.style.display = "flex";
             document.querySelector('header').style.position = "inherit";
-
+            document.querySelector('#inicio').style.paddingTop = '0px';
 
             if (especialidadeData) {
                 especialidadeData.forEach((medico) => {
@@ -58,7 +161,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     medicoElement.innerHTML = `
                         <h3>${medico.nome}</h3>
                         <img src="${medico.foto}" alt="${medico.nome}">
-                        <!-- Adicione mais informações conforme necessário -->
                     `;
                     medicosInfo.appendChild(medicoElement);
                 });
@@ -72,7 +174,7 @@ document.addEventListener("DOMContentLoaded", function () {
             medicosBackground.style.display = "none";
             document.body.style.overflowY = "visible";
             document.querySelector('header').style.position = "fixed";
+            document.querySelector('#inicio').style.paddingTop = '84px';
         }
     });
 });
-
